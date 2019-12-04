39c36b987caae039d66b65f3c714f9de6639805d
Roger Mayer <rmayer1984@gmail.com>
2019-12-04 15:54:03 -0600

    completed java tic tac toe

---
+++ src/Game/TicTacToe.java
@@ -0,0 +1,173 @@
+package Game;
+import java.util.Scanner;
+
+
+public class TicTacToe {
+    private int counter;
+    private char[] posn = new char[10];
+    private char player;
+
+
+    public static void main(String args[])
+    {
+        String ch;
+        TicTacToe Toe = new TicTacToe();
+        do{
+            Toe.newBoard();
+            Toe.play();
+            System.out.println ("Would you like to play again (Enter 'yes')? ");
+            Scanner in =new Scanner(System.in);
+            ch=in.nextLine();
+            System.out.println("ch value is  "+ch);
+        }while (ch.equals("yes"));
+
+
+    }
+
+    public  void newBoard()
+    {
+
+        char posndef[] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        int i;
+        counter = 0;
+        player = 'X';
+        for (i=1; i<10; i++) posn[i]=posndef[i];
+        currentBoard();
+
+
+    }
+    public  String currentBoard()
+    {
+        System.out.println(  "\n" );
+        System.out.println(" __________________");
+        System.out.println("|  -" + posn [1] + "- | -" + posn [2] + "- | -" + posn [3] + "- |");
+        System.out.println(" __________________");
+        System.out.println("|  -" + posn [4] + "- | -" + posn [5] + "- | -" + posn [6] + "- |");
+        System.out.println(" __________________");
+        System.out.println("|  -" + posn [7] + "- | -" + posn [8] + "- | -" + posn [9] + "- |");
+        System.out.println(" __________________");
+        System.out.println(  "\n" );
+        return "currentBoard";
+    }
+
+    public  void play()
+    {
+        int spot;
+        char blank = ' ';
+
+        System.out.println(  "Player " + getPlayer() +" will go first and be the letter 'X'" );
+
+        do {
+//            currentBoard();              // display current board
+
+            System.out.println(  "Player " + getPlayer() +" choose a square." );
+
+            boolean posTaken = true;
+            while (posTaken) {
+//                System.out.println( "position is taken, please enter a valid space");
+                Scanner in =new Scanner (System.in);
+                spot=in.nextInt();
+                posTaken = checkPosn(spot);
+                if(posTaken==false)
+                    posn[spot]=getPlayer();
+            }
+
+            System.out.println(  "Nice move." );
+
+            currentBoard();              // display current board
+
+            nextPlayer();
+        }while ( checkWinner() == blank );
+
+    }
+
+    public  char checkWinner()
+    {
+        char Winner = ' ';
+
+        // Check if X wins
+        if (posn[1] == 'X' && posn[2] == 'X' && posn[3] == 'X') Winner = 'X';
+        if (posn[4] == 'X' && posn[5] == 'X' && posn[6] == 'X') Winner = 'X';
+        if (posn[7] == 'X' && posn[8] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[1] == 'X' && posn[4] == 'X' && posn[7] == 'X') Winner = 'X';
+        if (posn[2] == 'X' && posn[5] == 'X' && posn[8] == 'X') Winner = 'X';
+        if (posn[3] == 'X' && posn[6] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[1] == 'X' && posn[5] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[3] == 'X' && posn[5] == 'X' && posn[7] == 'X') Winner = 'X';
+        if (Winner == 'X' )
+        {
+            System.out.println("Player1 wins the game." );
+            System.out.println("*********************** \n");
+            return Winner;
+        }
+
+        // Check if O wins
+        if (posn[1] == 'O' && posn[2] == 'O' && posn[3] == 'O') Winner = 'O';
+        if (posn[4] == 'O' && posn[5] == 'O' && posn[6] == 'O') Winner = 'O';
+        if (posn[7] == 'O' && posn[8] == 'O' && posn[9] == 'O') Winner = 'O';
+        if (posn[1] == 'O' && posn[4] == 'O' && posn[7] == 'O') Winner = 'O';
+        if (posn[2] == 'O' && posn[5] == 'O' && posn[8] == 'O') Winner = 'O';
+        if (posn[3] == 'O' && posn[6] == 'O' && posn[9] == 'O') Winner = 'O';
+        if (posn[1] == 'O' && posn[5] == 'O' && posn[9] == 'O') Winner = 'O';
+        if (posn[3] == 'O' && posn[5] == 'O' && posn[7] == 'O') Winner = 'O';
+        if (Winner == 'O' )
+        {
+            System.out.println( "Player2 wins the game." );
+            System.out.println("*********************** \n");
+            return Winner; }
+
+        // check for Tie
+        for(int i=1;i<10;i++)
+        {
+            if(posn[i]=='X' || posn[i]=='O')
+            {
+                if(i==9)
+                {
+                    char Draw='D';
+                    System.out.println(" Game is stalemate ");
+                    return Draw;
+                }
+                continue;
+            }
+            else
+                break;
+
+        }
+
+        return Winner;
+    }
+
+    public  boolean checkPosn(int spot)
+    {
+
+
+        if (posn[spot] == 'X' || posn[spot] == 'O')
+        {
+            System.out.println("That square is already taken, please choose another");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public  void nextPlayer()
+    {
+        if (player == 'X')
+            player = 'O';
+        else player = 'X';
+
+    }
+
+    public String getTitle()
+    {
+        return "Tic Tac Toe" ;
+    }
+
+    public  char getPlayer()
+    {
+        return player;
+    }
+
+}
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 public class MoviesApplication {
 
 
-        public static Movie[] addMovie(Movie[] movieArray, Movie newMovie) {
-            movieArray = Arrays.copyOf(movieArray, movieArray.length + 1);
-            movieArray[movieArray.length-1] = newMovie;
-            return movieArray;
-        }
+    public static Movie[] addMovie(Movie[] movieArray, Movie newMovie) {
+        movieArray = Arrays.copyOf(movieArray, movieArray.length + 1);
+        movieArray[movieArray.length - 1] = newMovie;
+        return movieArray;
+    }
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Input in = new Input();
         Movie[] newMovies = MoviesArray.findAll();
         System.out.println();
@@ -28,44 +28,44 @@ public class MoviesApplication {
                 "5 - view movies in the scifi category\n" +
                 "6 - add movie\n");
 
-        int userInput = in.getInt(0,6);
+        int userInput = in.getInt(0, 6);
 
-        while (true){
-            if (userInput == 0){
+        while (true) {
+            if (userInput == 0) {
                 System.out.println("See you later");
                 System.exit(0);
-            } else if (userInput == 1){
+            } else if (userInput == 1) {
                 System.out.println("Here are all the movies");
-                for(Movie movie : newMovies ) {
+                for (Movie movie : newMovies) {
                     System.out.println(movie.getName() + " " + movie.getCategory());
-                    }
-            } else if (userInput == 2){
+                }
+            } else if (userInput == 2) {
                 System.out.println("Here are all the animated movies");
-                for(Movie movie : newMovies ) {
+                for (Movie movie : newMovies) {
                     if (movie.getCategory().equals("animated"))
-                    System.out.println(movie.getName() + ": " + movie.getCategory());
+                        System.out.println(movie.getName() + ": " + movie.getCategory());
 
                 }
 
-            } else if (userInput == 3){
+            } else if (userInput == 3) {
                 System.out.println("Here are all the drama movies");
-                for(Movie movie : newMovies ) {
+                for (Movie movie : newMovies) {
                     if (movie.getCategory().equals("drama"))
                         System.out.println(movie.getName() + ": " + movie.getCategory());
                 }
-            } else if (userInput == 4){
+            } else if (userInput == 4) {
                 System.out.println("Here are all the horror movies");
-                for(Movie movie : newMovies ) {
+                for (Movie movie : newMovies) {
                     if (movie.getCategory().equals("horror"))
                         System.out.println(movie.getName() + ": " + movie.getCategory());
                 }
-            }else if (userInput == 5){
+            } else if (userInput == 5) {
                 System.out.println("Here are all the scifi movies");
-                for(Movie movie : newMovies ) {
+                for (Movie movie : newMovies) {
                     if (movie.getCategory().equals("scifi"))
                         System.out.println(movie.getName() + ": " + movie.getCategory());
-                    }
-                } else if(userInput == 6){
+                }
+            } else if (userInput == 6) {
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Enter movie title \n");
                 String input1 = sc.next();
@@ -86,11 +86,8 @@ public class MoviesApplication {
                     "5 - view movies in the scifi category\n" +
                     "6 - add movie\n");
 
-            userInput = in.getInt(0,6);
+            userInput = in.getInt(0, 6);
         }
     }
-//    public static void main(String[] args) {
-//        movie();
-//    }
-//test
+
 }
